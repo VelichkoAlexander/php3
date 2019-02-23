5f39fbaea619462fbb4f3946fc3ab64310d20b52
Alexander Velichko <alvi@mail.ua>
2019-02-23 14:11:47 +0200

    initial commit

---
+++ src/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Generic_Sniffs_Methods_OpeningMethodBraceBsdAllmanSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Alexander Velichko <alvi@mail.ua>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: OpeningFunctionBraceBsdAllmanSniff.php,v 1.8
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
